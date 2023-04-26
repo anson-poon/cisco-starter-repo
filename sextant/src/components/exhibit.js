43162baf8ff53139b441c6a5beaf226f9c241445
Anson Poon <ansonpoon77@gmail.com>
2023-04-25 19:59:47 -0700

    Added banner and exhibit components ```

---
+++ sextant/src/components/exhibit.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Exhibit() {
+  return (
+    <div className="Exhibit">
+      <div>
+          <p className="ExhibitCard">Data point 1</p>
+          <p className="ExhibitCard">Data point 2</p>
+          <p className="ExhibitCard">Data point 3</p>
+          <p className="ExhibitCard">Data point 4</p>
+          <p className="ExhibitCard">Data point 5</p>
+          <p className="ExhibitCard">Data point 6</p>
+          <p className="ExhibitCard">Data point 7</p>
+          <p className="ExhibitCard">Data point 8</p>
+          <p className="ExhibitCard">Data point 9</p>
+      </div>
+    </div>
+  );
+}
+
+export default Exhibit;
